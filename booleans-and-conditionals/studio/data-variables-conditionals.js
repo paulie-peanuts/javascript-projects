c094c8189fc5fb6b9d57ca74372caed6e301be9d
PauliePnuts <91554001+PauliePnuts@users.noreply.github.com>
2024-03-27 15:28:51 -0500

    studio done

--- booleans-and-conditionals/studio/data-variables-conditionals.js
+++ booleans-and-conditionals/studio/data-variables-conditionals.js
@@ -1,6 +1,49 @@
 // Initialize Variables below
+let date = "Monday 2019-03-18";
+let time = "10:05:34 AM";
+let astronautCount = 7;
+let astronautStatus = "ready";
+let averageAstronautMassKg = 80.7;
+let crewMassKg = astronautCount * averageAstronautMassKg;
+let fuelMassKg = 760000;
+let shuttleMassKg = 74842.31;
+let totalMassKg = crewMassKg + fuelMassKg + shuttleMassKg;
+let maximumMassLimit = 850000;
+let fuelTempCelsius = -225;
+let minimumFuelTemp = -300;
+let maximumFuelTemp = -150;
+let fuelLevel = "100%";
+let weatherStatus = "clear";
+let preparedForLiftOff = true;
 
+let failText = "Shut down the launch operations!";
 // add logic below to verify total number of astronauts for shuttle launch does not exceed 7
+if (astronautCount > 7) {
+    console.log(failText);
+} else if (astronautStatus !== "ready") {
+    console.log(failText);
+} else if (totalMassKg >= maximumMassLimit) {
+    console.log(failText);
+} else if (fuelTempCelsius < minimumFuelTemp || fuelTempCelsius > maximumFuelTemp) {
+    console.log(failTest);
+} else if (fuelLevel !== "100%") {
+    console.log(failTest);
+} else if (weatherStatus !== "clear") {
+    console.log(failTest);
+} else console.log (
+    "All systems are a go! Initiating space shuttle launch sequence." +
+    "\n---------------------------------------------------------------" +
+    "\nDate: " + date + 
+    "\nTime: " + time + 
+    "\nAstronaut Count: " + astronautCount + 
+    "\nCrew Mass: " + crewMassKg + " kg" + 
+    "\nFuel Mass: " + fuelMassKg + " kg" + 
+    "\nShuttle Mass: " + shuttleMassKg + " kg" + 
+    "\nTotal Mass: " + totalMassKg + " kg" +
+    "\nFuel Temperature: " + fuelTempCelsius + " degrees C" +
+    "\nWeather Status: " + weatherStatus +
+    "\n---------------------------------------------------------------" +
+    "\nHave a safe trip astronauts!")
 
 // add logic below to verify all astronauts are ready
 
@@ -12,4 +55,4 @@
 
 // add logic below to verify the weather status is clear
 
-// Verify shuttle launch can proceed based on above conditions
+// Verify shuttle launch can proceed based on above conditions 
