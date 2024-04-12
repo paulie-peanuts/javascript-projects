d363366c27265aae56551c5c5ee48da97dbd3023
PauliePnuts <91554001+PauliePnuts@users.noreply.github.com>
2024-04-11 21:43:10 -0500

    studio done

--- unit-testing/studio/index.js
+++ unit-testing/studio/index.js
@@ -1,7 +1,39 @@
 
 let launchcode = {
-  
-}
+  organization: 'nonprofit',
+  executiveDirector: 'Jeff',
+  percentageCoolEmployees: 100,
+  programsOffered: ["Web Development","Data Analysis","Liftoff"],
+  launchOutput: function (number){
+    let output = ''
+    let object = {2: 'Launch', 3: 'Code', 5: 'Rocks'}
+    // for () {
 
+    // }
+    if (number % 2 === 0) {
+        output += 'Launch'
+    }    
+    if (number % 3 === 0) {
+        output += 'Code'
+    }    
+    if (number % 5 === 0) {
+        output += 'Rocks' 
+    }   
+    if (number % 15 === 0) {
+        output = 'Code Rocks' 
+    }        
+    if (number % 30 === 0) {
+        return 'LaunchCode Rocks!' 
+    } 
+    if (number % 10 === 0) {
+        return 'Launch Rocks! (CRASH!!!!!)' 
+    }   
+    if (number % 2 !== 0 && number % 3 !== 0 && number % 5 !== 0) {
+        return "Rutabagas! That doesn't work."
+    }
+    return output + "!"
+}
+}
+console.log(launchcode.launchOutput(1))
 module.exports = launchcode;
 
--- unit-testing/studio/tests/launchcode.test.js
+++ unit-testing/studio/tests/launchcode.test.js
@@ -2,7 +2,45 @@
 const launchcode = require('../index.js');
 
 describe("Testing launchcode", function(){
-
+  test('should have key called organization with the value of nonprofit', function() {
+    expect(launchcode.organization).toBe('nonprofit')
+  });
+  test('should have key called executiveDirector with the value of Jeff', function () {
+    expect(launchcode.executiveDirector).toBe('Jeff')
+  });
+  test('should have key called percentageCoolEmployees with value of 100', function () {
+    expect(launchcode.percentageCoolEmployees === 100).toBe(true)
+  });
+  test('should have key called programsOffered with stores an array "Web Development", "Data Analysis", "Liftoff"', function() {
+    expect(launchcode.programsOffered.length).toBe(3);
+    expect(launchcode.programsOffered[0]).toBe('Web Development');
+    expect(launchcode.programsOffered[1]).toBe('Data Analysis');
+    expect(launchcode.programsOffered[2]).toBe('Liftoff');
+  });
+  test('should have function called launchOutput that returns "Launch!" when a number is ONLY divisible by 2', function () {
+    expect(launchcode.launchOutput(2)).toBe('Launch!');
+  })
+  test('should have function called launchOutput that returns "Code!" when a number is ONLY divisible by 3', function () {
+    expect(launchcode.launchOutput(3)).toBe('Code!');
+  })
+  test('should have function called launchOutput that returns "Rocks!" when a number is ONLY divisible by 5', function () {
+    expect(launchcode.launchOutput(5)).toBe('Rocks!');
+  })
+  test('should have function called launchOutput that returns "LaunchCode!" when a number is divisible by 2 and 3', function () {
+    expect(launchcode.launchOutput(6)).toBe('LaunchCode!');
+  })  
+  test('should have function called launchOutput that returns "Code Rocks!" when a number is divisible by 3 and 5', function () {
+    expect(launchcode.launchOutput(15)).toBe('Code Rocks!');
+  })  
+  test('should have function called launchOutput that returns "Launch Rocks! (CRASH!!!!!)" when a number is divisible by 2 and 5', function () {
+    expect(launchcode.launchOutput(10)).toBe('Launch Rocks! (CRASH!!!!!)');
+  })  
+  test('should have function called launchOutput that returns "LaunchCode Rocks!" when a number is divisible by 2, 3, and 5', function () {
+    expect(launchcode.launchOutput(30)).toBe('LaunchCode Rocks!');
+  }) 
+  test("should have function called launchOutput that returns 'Rutabagas! That doesn't work.' when a number is NOT divisible by 2, 3, or 5", function () {
+    expect(launchcode.launchOutput(1)).toBe("Rutabagas! That doesn't work.");
+  }) 
   // Write your unit tests here!
   
 });
