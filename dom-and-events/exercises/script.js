b72f791e1089562346c6fd5806c198bbd3f3362a
PauliePnuts <91554001+PauliePnuts@users.noreply.github.com>
2024-04-23 15:44:08 -0500

    exercises done

--- dom-and-events/exercises/script.js
+++ dom-and-events/exercises/script.js
@@ -3,8 +3,20 @@ function init () {
     const button = document.getElementById("liftoffButton");
     const paragraph = document.getElementById("statusReport");
 
-    // Put your code for the exercises here.
-    
+    button.addEventListener('click', event => {
+        paragraph.innerHTML = 'Houston! We have liftoff!';
+    });
+    missionAbort.addEventListener('mouseover', function() {
+        missionAbort.style.background = "red";
+    });
+    missionAbort.addEventListener('mouseout', function() {
+        missionAbort.style.background = "";
+    });  
+    missionAbort.addEventListener('click', function() {
+        if (confirm('Are you sure you want to abort the mission?')) {
+            paragraph.innerHTML = 'Mission aborted! Space shuttle returning home';
+        }
+    });   
 }
 
 window.addEventListener("load", init);
